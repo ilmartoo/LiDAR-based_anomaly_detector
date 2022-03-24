3b39ff4d0717b6950116b150ab8550cc9b0b086f
ilMarto <martinszga@gmail.com>
2022-03-24 14:42:50 +0100

    Added reflectivity

--- include/models/Point.hpp
+++ include/models/Point.hpp
@@ -21,17 +21,39 @@ class Point {
     /**
      * Constructor del objeto Punto
      * @param timestamp Timestamp del punto
+     * @param reflectivity Reflectividad del punto
      * @param x Posición en x del punto
      * @param y Posición en y del punto
      * @param z Posición en z del punto
      */
-    Point(uint64_t timestamp, int32_t x, int32_t y, int32_t z) : timestamp(timestamp), x(x), y(y), z(z) {}
+    Point(uint64_t timestamp, uint8_t reflectivity, int32_t x, int32_t y, int32_t z)
+        : timestamp(timestamp), reflectivity(reflectivity), x(x), y(y), z(z) {
+        this->timestamp_counter = 0;  // Solo se ejecuta la primera vez
+    }
+
+    /**
+     * Constructor del objeto Punto
+     * @param reflectivity Reflectividad del punto
+     * @param x Posición en x del punto
+     * @param y Posición en y del punto
+     * @param z Posición en z del punto
+     */
+    Point(uint8_t reflectivity, int32_t x, int32_t y, int32_t z) : reflectivity(reflectivity), x(x), y(y), z(z) {
+        this->timestamp_counter = 0;  // Solo se ejecuta la primera vez
+        this->timestamp = timestamp_counter++;
+    }
 
     /**
      * Devuelve el timestamp del punto
      * @return Timestamp del punto
      */
     inline uint64_t getTimestamp() { return this->timestamp; };
+
+    /**
+     * Devuelve la reflectividad del punto
+     * @return Reflectividad del punto
+     */
+    inline uint8_t getReflectivity() { return this->reflectivity; };
 
     /**
      * Devuelve la localización en el eje x del punto
@@ -53,14 +75,17 @@ class Point {
 
     // Imprime la información del punto p
     friend std::ostream &operator<<(std::ostream &strm, const Point &p) {
-        return strm << "[" << p.timestamp << "] " << p.x << ", " << p.y << ", " << p.z;
+        return strm << "[Time: " << p.timestamp << "] [Ref: " << p.reflectivity << "] " << p.x << ", " << p.y << ", "
+                    << p.z;
     }
 
    private:
-    uint64_t timestamp;  ///< Timestamp del punto
-    int32_t x;           ///< Localización en el eje x del punto
-    int32_t y;           ///< Localización en el eje y del punto
-    int32_t z;           ///< Localización en el eje <x> del punto
+    static uint64_t timestamp_counter;  ///< Contador del timestamp del punto
+    uint64_t timestamp;                 ///< Timestamp del punto
+    uint8_t reflectivity;               ///< Reflectividad del punto
+    int32_t x;                          ///< Localización en el eje x del punto
+    int32_t y;                          ///< Localización en el eje y del punto
+    int32_t z;                          ///< Localización en el eje <x> del punto
 };
 
 #endif  //__POINT_CLASS_H
