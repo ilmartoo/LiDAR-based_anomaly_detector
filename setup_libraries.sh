ae70a8d79716e98a02beb9b50af6ddf6552bb930
ilMarto <martinszga@gmail.com>
2022-04-06 20:54:56 +0200

    Improved library compilation

--- setup_libraries.sh
+++ setup_libraries.sh
@@ -1,27 +1,31 @@
-cd lib/ # Entramos a la carpeta de librerías
-
 # Crea la librería sdk_core
-if [ ! -r /usr/local/lib/liblivox_sdk_static.a ]; then
-	cd sdk_core/ && mkdir -p build && cd build && cmake .. && make && sudo make install && cd ../../
-	echo -e "\e[1;34m[sdk_core] Librería sdk_core creada\e[0m"
+if [ ! -r lib/sdk_core/lib/liblivox_sdk_static.a ]; then
+	echo -e "\e[1;33m[sdk_core] Creando...\e[0m"
+	cmake -S lib/sdk_core -B lib/sdk_core/build
+	cmake --build lib/sdk_core/build -- install
+	echo -e "\e[1;32m[sdk_core] Librería creada\e[0m"
 else
-	echo -e "\e[1;34m[sdk_core] Librería sdk_core ya disponible\e[0m"
-fi
-
-# Crea la librería googletest
-if [ ! -r /usr/local/lib/libgmock.a ] && [ ! -r /usr/local/lib/libgtest.a ]; then
-	cd googletest/ && mkdir -p build && cd build && cmake .. && make && sudo make install && cd ../../
-	echo -e "\e[1;34m[sdk_core] Librerías gtest y gmock creadas\e[0m"
-else
-	echo -e "\e[1;34m[sdk_core] Librerías gtest y gmock ya disponibles\e[0m"
+	echo -e "\e[1;32m[sdk_core] Librería ya disponible\e[0m"
 fi
 
 # Crea la librería read_lvx
-if [ ! -r /usr/local/lib/libread_lvx.a ]; then
-	cd read_lvx/ && mkdir -p build && cd build && cmake .. && make && sudo make install && cd ../../
-	echo -e "\e[1;34m[sdk_core] Librería read_lvx creada\e[0m"
+if [ ! -r lib/read_lvx/lib/libread_lvx.a ]; then
+	echo -e "\e[1;33m[read_lvx] Creando...\e[0m"
+	cmake -S lib/read_lvx -B lib/read_lvx/build
+	cmake --build lib/read_lvx/build -- install
+	echo -e "\e[1;32m[read_lvx] Librería creada\e[0m"
 else
-	echo -e "\e[1;34m[sdk_core] Librería read_lvx ya disponible\e[0m"
+	echo -e "\e[1;32m[read_lvx] Librería ya disponible\e[0m"
 fi
 
-cd ../ # Salimos de la carpeta de librerías
+if [ $# = 1 ] && [ $1 = "-t" ]; then
+	# if [ ! -r lib/read_lvx/lib/libread_lvx.a ]; then
+	# 	echo -e "\e[1;33m[gtest] Creando...\e[0m"
+	# 	cmake -S lib/read_lvx -B lib/read_lvx/build
+	# 	cmake --build lib/read_lvx/build -- install
+	# 	echo -e "\e[1;32m[gtest] Librería creada\e[0m"
+	# else
+	# 	echo -e "\e[1;32m[gtest] Librería ya disponible\e[0m"
+	# fi
+	echo g
+fi
