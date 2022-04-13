bfeb769aed1cb4f18223da1d086e8856c41f434c
ilMarto <martinszga@gmail.com>
2022-04-13 14:32:34 +0200

    Added library version

--- anomaly_detector/setup_libraries.sh
+++ anomaly_detector/setup_libraries.sh
@@ -18,15 +18,16 @@ else
 	echo -e "\e[1;32m[read_lvx] Librería ya disponible\e[0m"
 fi
 
+# Creas las librerías googletest y googlemock
 if [ $# = 1 ] && [ $1 = "-t" ]; then
-	if [ ! -r lib/googletest/lib/libtest_main.a ] || [ ! -r lib/googletest/lib/libtest.a ] || [ ! -r lib/googletest/lib/libmock_main.a ] || [ ! -r lib/googletest/lib/libmock.a ]; then
+	if [ ! -r lib/googletest-1.11.0/lib/libtest_main.a ] || [ ! -r lib/googletest-1.11.0/lib/libtest.a ] || [ ! -r lib/googletest-1.11.0/lib/libmock_main.a ] || [ ! -r lib/googletest-1.11.0/lib/libmock.a ]; then
 		echo -e "\e[1;33m[gtest & gmock] Creando...\e[0m"
-		cmake -S lib/googletest -B lib/googletest/build
-		cmake --build lib/googletest/build
-		mkdir -p lib/googletest/lib/include/
-		mv lib/googletest/build/lib/* lib/googletest/lib/
-		cp -r lib/googletest/googletest/include/* lib/googletest/lib/include/
-		cp -r lib/googletest/googlemock/include/* lib/googletest/lib/include/
+		cmake -S lib/googletest-1.11.0 -B lib/googletest-1.11.0/build
+		cmake --build lib/googletest-1.11.0/build
+		mkdir -p lib/googletest-1.11.0/lib/include/
+		mv lib/googletest-1.11.0/build/lib/* lib/googletest-1.11.0/lib/
+		cp -r lib/googletest-1.11.0/googletest/include/* lib/googletest-1.11.0/lib/include/
+		cp -r lib/googletest-1.11.0/googlemock/include/* lib/googletest-1.11.0/lib/include/
 		echo -e "\e[1;32m[gtest & gmock] Librerías creadas\e[0m"
 	else
 		echo -e "\e[1;32m[gtest & gmock] Librerías ya disponibles\e[0m"
