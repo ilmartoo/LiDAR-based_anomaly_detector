2794bcdb72a21ddffdef3860588279e4fd611e39
ilMarto <martinszga@gmail.com>
2022-04-13 13:13:45 +0200

    Added googletest to setup_libraries.sh

--- anomaly_detector/setup_libraries.sh
+++ anomaly_detector/setup_libraries.sh
@@ -19,13 +19,16 @@ else
 fi
 
 if [ $# = 1 ] && [ $1 = "-t" ]; then
-	# if [ ! -r lib/read_lvx/lib/libread_lvx.a ]; then
-	# 	echo -e "\e[1;33m[gtest] Creando...\e[0m"
-	# 	cmake -S lib/read_lvx -B lib/read_lvx/build
-	# 	cmake --build lib/read_lvx/build -- install
-	# 	echo -e "\e[1;32m[gtest] Librería creada\e[0m"
-	# else
-	# 	echo -e "\e[1;32m[gtest] Librería ya disponible\e[0m"
-	# fi
-	echo g
+	if [ ! -r lib/googletest/lib/libtest_main.a ] || [ ! -r lib/googletest/lib/libtest.a ] || [ ! -r lib/googletest/lib/libmock_main.a ] || [ ! -r lib/googletest/lib/libmock.a ]; then
+		echo -e "\e[1;33m[gtest & gmock] Creando...\e[0m"
+		cmake -S lib/googletest -B lib/googletest/build
+		cmake --build lib/googletest/build
+		mkdir -p lib/googletest/lib/include/
+		mv lib/googletest/build/lib/* lib/googletest/lib/
+		cp -r lib/googletest/googletest/include/* lib/googletest/lib/include/
+		cp -r lib/googletest/googlemock/include/* lib/googletest/lib/include/
+		echo -e "\e[1;32m[gtest & gmock] Librerías creadas\e[0m"
+	else
+		echo -e "\e[1;32m[gtest & gmock] Librerías ya disponibles\e[0m"
+	fi
 fi
--- anomaly_detector/src/test/parser_test.cc
+++ anomaly_detector/src/test/parser_test.cc
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include "app/InputParser.hh"
+
 int main(int argc, char* argv[]) {
 
 	
