73dab23a25d440603e260d99e45a40e8310a0744
ilMarto <martinszga@gmail.com>
2022-04-16 21:27:11 +0200

    Fix

--- anomaly_detector/include/debug.hh
+++ anomaly_detector/include/debug.hh
@@ -10,24 +10,11 @@
 #ifndef DEBUG_HEADER_H
 #define DEBUG_HEADER_H
 
-////////////////////////////
-// Inutilizamos las macros de debugging
-#define DEBUG_CODE(x)
-#define DEBUG_STDOUT(x)
-#define DEBUG_STDOUT(x)
-#define DEBUG_STDERR(x)
-#define DEBUG_POINT_STDOUT(x)
-#define DEBUG_POINT_STDERR(x)
-////////////////////////////
-
 #include <iostream>
 #include <string>
 
 #include "app/string_format.h"
 
-////////////////////////////
-
-// Redefinimos las macros de debugging si procede
 #ifdef DEBUG
 #define DEBUG_CODE(x) x
 #define DEBUG_STDOUT(x) do { std::cout << yellow_s("[DEBUG] " + std::string(x)) << std::endl; } while (0)
@@ -36,9 +23,17 @@
 #ifdef DEBUG_POINTS
 #define DEBUG_POINT_STDOUT(x) do { std::cout << yellow_s("[DEBUG] " + std::string(x)) << std::endl; } while (0)
 #define DEBUG_POINT_STDERR(x) do { std::cerr << red_s("[DEBUG] " + std::string(x)) << std::endl; } while (0)
-
+#else // DEBUG_POINTS
+#define DEBUG_POINT_STDOUT(x)
+#define DEBUG_POINT_STDERR(x)
 #endif  // DEBUG_POINTS
+
+#else // DEBUG
+#define DEBUG_CODE(x)
+#define DEBUG_STDOUT(x)
+#define DEBUG_STDERR(x)
+#define DEBUG_POINT_STDOUT(x)
+#define DEBUG_POINT_STDERR(x)
 #endif  // DEBUG
-////////////////////////////
 
 #endif  // DEBUG_HEADER_H
